52c7d857f6d71ff994b1f4cafb71b4b2b1f3e1e9
KaleemAhmed123 <shaamidreez@gmail.com>
2023-02-23 23:48:11 +0530

    Details Component done .

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/usefetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
 import "./style.scss";
 
 const Details = () => {
-  return <div>Details</div>;
+  // const {mediaType, id} = useParams();
+  // const {data, loading} = useFetch(`/${mediaType}/${id}`)
+
+  return (
+    <div>
+      <DetailsBanner />
+    </div>
+  );
 };
 
 export default Details;
